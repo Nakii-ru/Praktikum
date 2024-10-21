49b8b56295091a725d0ba5d6fa76c4217dab4fee
Ruu <arueqy7@gmail.com>
2024-10-21 13:29:33 +0700

    Update praktikum1.py

--- praktikum1.py
+++ praktikum1.py
@@ -1,14 +1,15 @@
-# Inisiasi nilai MAX menjadi minus tak hingga
+# Inisialisasi nilai variabel MAX dengan minus tak hingga
 MAX = float('-inf')
 while True:
         # Input N dari pengguna
         N = int(input("Masukkan Nilai N: "))
         
-        # Cek apakah pengguna ingin keluar
+        # Cek apakah nilai yang dimasukkan adalah angka 0, jika iya, maka perulangan akan berakhir dan akan menunjukan angka terbesar yang sudah dimasukkan
         if N == 0:
             break
         
         # Cek apakah bilangan yang diinputkan lebih besar dari MAX 
         if N > MAX:
+        # Jika kondisi diatas sudah terpenuhi (N > MAX)
             MAX = N
-print(f"Bilangan Terbesar yang dimasukan adalah: {MAX}")
+print("Bilangan Terbesar yang dimasukan adalah: ", MAX)
